8cd6dc8f0f00417ce9d94a09000ed50d7ff992d4
adembacaj <adibacaj@live.com>
2020-06-13 00:27:39 +0200

    all file names changed from .js to .jsx

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
+import { authenticate } from '../../store/actions/authenticate.action';
 import './login.scss';
 
 const Login = (props) => {
+    const login = () => props.authenticate(true)
     return (
-        <div>Login.js</div>
+        <div>
+            <button onClick={login}>Login</button>
+        </div>
     )
 }
 
 const mapStateToProps = null;
-const mapDispatchToProps = null;
+const mapDispatchToProps = { authenticate };
 
 export default connect(mapStateToProps, mapDispatchToProps)(withRouter(Login));
--- src/store/index.jsx
+++ src/store/index.jsx
@@ -1,6 +1,6 @@
 import { createStore, applyMiddleware, compose } from "redux";
 import reduxThunk from "redux-thunk";
-import reducers from "./reducers/";
+import reducers from "./reducers";
 
 export default createStore(
     reducers,
