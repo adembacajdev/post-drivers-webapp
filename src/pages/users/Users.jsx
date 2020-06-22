faef516ec3d55d3f103a29d3c93985eac610181a
adembacaj <adibacaj@live.com>
2020-06-22 19:40:39 +0200

    make every item of table clickable

--- src/pages/users/Users.jsx
+++ src/pages/users/Users.jsx
@@ -12,23 +12,23 @@ const Customers = (props) => {
     const addUser = useCallback(() => {props.history.push('/add-user')}, [])
     return (
         <Wrapper>
-            <div className="strike-customers">
-                <div className="strike-customers__header">
-                    <div className="strike-customers__header-left">
-                        <div className="strike-customers__header-left-text">{i18n.t('users.title')}</div>
+            <div className="strike-users">
+                <div className="strike-users__header">
+                    <div className="strike-users__header-left">
+                        <div className="strike-users__header-left-text">{i18n.t('users.title')}</div>
                     </div>
-                    <div className="strike-customers__header-right">
-                        <div onClick={addUser} className="strike-customers__header-right-text">{i18n.t('users.addUser')}</div>
+                    <div className="strike-users__header-right">
+                        <div onClick={addUser} className="strike-users__header-right-text">{i18n.t('users.addUser')}</div>
                     </div>
                 </div>
                 <Table items={[1, 2, 3, 4, 5, 6]} />
-                <div className="strike-customers__pagination">
-                    <img src={unfilledLeftArrow} className='strike-customers__pagination-leftarrow' />
-                    <div is-active="true" className="strike-customers__pagination-number">1</div>
-                    <div className="strike-customers__pagination-number">2</div>
-                    <div className="strike-customers__pagination-number">3</div>
-                    <div className="strike-customers__pagination-number">4</div>
-                    <img src={filledRightArrow} className='strike-customers__pagination-rightArrow' />
+                <div className="strike-users__pagination">
+                    <img src={unfilledLeftArrow} className='strike-users__pagination-leftarrow' />
+                    <div is-active="true" className="strike-users__pagination-number">1</div>
+                    <div className="strike-users__pagination-number">2</div>
+                    <div className="strike-users__pagination-number">3</div>
+                    <div className="strike-users__pagination-number">4</div>
+                    <img src={filledRightArrow} className='strike-users__pagination-rightArrow' />
                 </div>
             </div>
         </Wrapper>
@@ -44,31 +44,31 @@ export default connect(mapStateToProps, mapDispatchToProps)(withRouter(Customers
 const Table = ({ items }) => {
     const { tableArrow } = images.customers;
     return (
-        <div className="strike-customers__table">
-            <div className="strike-customers__table-header">
-                <div is-responsive="true" className="strike-customers__table-header-item">
-                    <div className="strike-customers__table-header-item-text">{i18n.t('customers.id')}</div>
+        <div className="strike-users__table">
+            <div className="strike-users__table-header">
+                <div is-responsive="true" className="strike-users__table-header-item">
+                    <div className="strike-users__table-header-item-text">{i18n.t('customers.id')}</div>
                 </div>
-                <div className="strike-customers__table-header-item">
-                    <div className="strike-customers__table-header-item-text">{i18n.t('users.firstName')}</div>
+                <div className="strike-users__table-header-item">
+                    <div className="strike-users__table-header-item-text">{i18n.t('users.firstName')}</div>
                 </div>
-                <div className="strike-customers__table-header-item">
-                    <div className="strike-customers__table-header-item-text">{i18n.t('users.lastName')}</div>
+                <div className="strike-users__table-header-item">
+                    <div className="strike-users__table-header-item-text">{i18n.t('users.lastName')}</div>
                 </div>
-                <div className="strike-customers__table-header-item">
-                    <div className="strike-customers__table-header-item-text">{i18n.t('users.phone')}</div>
+                <div className="strike-users__table-header-item">
+                    <div className="strike-users__table-header-item-text">{i18n.t('users.phone')}</div>
                 </div>
-                <div is-responsive="true" className="strike-customers__table-header-item">
-                    <div className="strike-customers__table-header-item-text">{i18n.t('users.email')}</div>
+                <div is-responsive="true" className="strike-users__table-header-item">
+                    <div className="strike-users__table-header-item-text">{i18n.t('users.email')}</div>
                 </div>
-                <div is-responsive="true" className="strike-customers__table-header-item centered">
-                    <div className="strike-customers__table-header-item-text">{i18n.t('users.createdAt')}</div>
+                <div is-responsive="true" className="strike-users__table-header-item centered">
+                    <div className="strike-users__table-header-item-text">{i18n.t('users.createdAt')}</div>
                 </div>
-                <div is-responsive="true" className="strike-customers__table-header-item centered">
-                    <div className="strike-customers__table-header-item-text">{i18n.t('users.role')}</div>
+                <div is-responsive="true" className="strike-users__table-header-item centered">
+                    <div className="strike-users__table-header-item-text">{i18n.t('users.role')}</div>
                 </div>
-                <div is-responsive="true" className="strike-customers__table-header-item centered">
-                    <div className="strike-customers__table-header-item-text">{i18n.t('users.actions')}</div>
+                <div is-responsive="true" className="strike-users__table-header-item centered">
+                    <div className="strike-users__table-header-item-text">{i18n.t('users.actions')}</div>
                 </div>
             </div>
             {items && items.map(item => {
@@ -81,32 +81,32 @@ const Table = ({ items }) => {
 const Item = (props) => {
     const { threePoints } = images.customers;
     return (
-        <div className="strike-customers__table-item">
-            <div is-responsive="true" className="strike-customers__table-item-container">
-                <input className="strike-customers__table-item-container-checkbox" type="checkbox" />
-                <div className="strike-customers__table-item-container-text">{props.item}</div>
+        <div className="strike-users__table-item">
+            <div is-responsive="true" className="strike-users__table-item-container">
+                <input className="strike-users__table-item-container-checkbox" type="checkbox" />
+                <div className="strike-users__table-item-container-text">{props.item}</div>
             </div>
-            <div className="strike-customers__table-item-container">
-                <div className="strike-customers__table-item-container-text">Francesca</div>
+            <div className="strike-users__table-item-container">
+                <div className="strike-users__table-item-container-text">Francesca</div>
             </div>
-            <div className="strike-customers__table-item-container">
-                <div className="strike-customers__table-item-container-text">Metts</div>
+            <div className="strike-users__table-item-container">
+                <div className="strike-users__table-item-container-text">Metts</div>
             </div>
-            <div className="strike-customers__table-item-container">
-                <div className="strike-customers__table-item-container-text">049838871</div>
+            <div className="strike-users__table-item-container">
+                <div className="strike-users__table-item-container-text">049838871</div>
             </div>
-            <div is-responsive="true" className="strike-customers__table-item-container">
-                <div className="strike-customers__table-item-container-text">fran@gmail.com</div>
+            <div is-responsive="true" className="strike-users__table-item-container">
+                <div className="strike-users__table-item-container-text">fran@gmail.com</div>
             </div>
-            <div is-responsive="true" className="strike-customers__table-item-container centered">
-                <div className="strike-customers__table-item-container-text">23/06/2020</div>
+            <div is-responsive="true" className="strike-users__table-item-container centered">
+                <div className="strike-users__table-item-container-text">23/06/2020</div>
             </div>
-            <div is-responsive="true" className="strike-customers__table-item-container centered">
-                <div className="strike-customers__table-item-container-text">Manager</div>
+            <div is-responsive="true" className="strike-users__table-item-container centered">
+                <div className="strike-users__table-item-container-text">Manager</div>
             </div>
-            <div is-responsive="true" className="strike-customers__table-item-container centered">
+            <div is-responsive="true" className="strike-users__table-item-container centered">
                 <div class="dropdown">
-                    <img className="strike-customers__table-item-container-dots" src={threePoints} />
+                    <img className="strike-users__table-item-container-dots" src={threePoints} />
                     <div class="dropdown-content">
                         <div className="dropdown-content-text">{i18n.t('users.edit')}</div>
                         <div className="dropdown-content-text">{i18n.t('users.delete')}</div>
